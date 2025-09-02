2289a4b0572c8fd30ba28a95c6f7f3935b824cc4
Luiz <luizfilipedalboniriello@gmail.com>
2025-09-02 18:31:26 -0300

    atualizando a tela-registro

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,41 +6,24 @@ const Footer = () => {
     <footer className="site-footer">
       <div className="container">
         
-        {/* Seção Superior - Passa a Bola */}
-        <div className="footer-top">
+        {/* Seção com 2 Colunas */}
+        <div className="footer-main">
           <div className="row">
-            <div className="col-lg-6">
-              <h3 className="footer-logo">passa a bela</h3>
+            
+            {/* Coluna 1 - Passa a Bola */}
+            <div className="col-md-6">
+              <h3 className="footer-logo">passa a bola</h3>
               <p className="footer-description">
                 A plataforma completa para acompanhar o futebol feminino. 
                 Valorizando o talento e a paixão das mulheres no esporte.
               </p>
             </div>
-          </div>
-        </div>
 
-        {/* Divisor */}
-        <hr className="footer-divider" />
-
-        {/* Seção de Links - 2 Colunas */}
-        <div className="footer-middle">
-          <div className="row">
-            
-            {/* Coluna Partidas */}
+            {/* Coluna 2 - Sobre Nós */}
             <div className="col-md-6">
-              <h5 className="footer-title">Partidas</h5>
+              <h5 className="footer-title">MAIS</h5>
               <ul className="footer-links">
-                <li><a href="#todas">Todas as partidas</a></li>
-                <li><a href="#calendario">Calendário</a></li>
-                <li><a href="#estatisticas">Estatísticas</a></li>
-              </ul>
-            </div>
-
-            {/* Coluna Sobre */}
-            <div className="col-md-6">
-              <h5 className="footer-title">Sobre Nós</h5>
-              <ul className="footer-links">
-                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#sobre">Sobre Nós</a></li>
                 <li><a href="#contato">Contato</a></li>
                 <li><a href="#duvidas">Dúvidas</a></li>
               </ul>
@@ -57,7 +40,7 @@ const Footer = () => {
           <div className="row">
             <div className="col-12">
               <p className="copyright">
-                © 2025 Passa a Bela. Todos os direitos reservados.
+                © 2025 Passa a Bola. Todos os direitos reservados.
               </p>
               <div className="legal-links">
                 <a href="#privacidade">Política de Privacidade</a>
