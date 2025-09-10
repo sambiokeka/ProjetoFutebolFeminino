1fd98623c3e3097e3e5c3cc2efb31fca5ed6d381
Erick <yamassoli@gmail.com>
2025-09-10 15:45:37 -0300

    mudança no main.jsx de app.jsx para partidas.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Partidas from './components/Partidas.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Partidas />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
